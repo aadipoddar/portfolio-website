2c659ec997142ed69fa83079a80da5a3d2cc5b49
Aadi Poddar <83405769+aadipoddar@users.noreply.github.com>
2022-02-10 00:09:27 +0530

    Fixed Whatsapp download Link

--- pages/works/whatsapp.js
+++ pages/works/whatsapp.js
@@ -28,13 +28,6 @@ const Work = () => (
 
             <List ml={4} my={4}>
                 <ListItem>
-                    <Meta>Source</Meta>
-                    <Link href="https://github.com/aadipoddar/WhatsAppAndroidStudio" target="_blank">
-                        github/whatsapp <ExternalLinkIcon mx="2px" />
-                    </Link>
-                </ListItem>
-
-                <ListItem>
                     <Meta>Platform</Meta>
                     <span>Android</span>
                 </ListItem>
@@ -43,12 +36,19 @@ const Work = () => (
                     <Meta>Stack</Meta>
                     <span>Java, XAML, Firebase</span>
                 </ListItem>
+
+                <ListItem>
+                    <Meta>Source</Meta>
+                    <Link href="https://github.com/aadipoddar/WhatsAppAndroidStudio" target="_blank">
+                        github/whatsapp <ExternalLinkIcon mx="2px" />
+                    </Link>
+                </ListItem>
             </List>
 
             <Box align="center" my={6}>
                 <Link
                     className="link-appstore"
-                    href="https://objects.githubusercontent.com/github-production-release-asset-2e65be/401710347/0ef2d3d9-850b-4226-9ec5-c25105f5a3fd?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220122%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220122T123324Z&X-Amz-Expires=300&X-Amz-Signature=da1a27893028c515aae641cf49f474f29f3cdaa6bb63880ee6b7f4f696d12fed&X-Amz-SignedHeaders=host&actor_id=83405769&key_id=0&repo_id=401710347&response-content-disposition=attachment%3B filename%3Dapp-release.apk&response-content-type=application%2Fvnd.android.package-archive"
+                    href="https://objects.githubusercontent.com/github-production-release-asset-2e65be/401710347/0ef2d3d9-850b-4226-9ec5-c25105f5a3fd?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220209T183752Z&X-Amz-Expires=300&X-Amz-Signature=9bd4dfc18ac22d1405fd6e5ceea1fd3c91070cc20d5f4a9cea3b3ef90d2bde6c&X-Amz-SignedHeaders=host&actor_id=83405769&key_id=0&repo_id=401710347&response-content-disposition=attachment%3B filename%3Dapp-release.apk&response-content-type=application%2Fvnd.android.package-archive"
                     target="_blank"
                 >
                     <Image
